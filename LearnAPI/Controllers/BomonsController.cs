1d17de67dc27d27c414b3f9e89874560e7466e5b
mizhhieuIT <dominhhieu.IT@gmail.com>
2021-05-21 17:06:16 +0700

    lan thu 2 biet api

---
+++ LearnAPI/Controllers/BomonsController.cs
@@ -0,0 +1,80 @@
+﻿using LearnAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearnAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BomonsController : ControllerBase
+    {
+        private readonly quanlycanbohumgContext _context; 
+        public BomonsController(quanlycanbohumgContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<IEnumerable<Bomon>> GetBomons()
+        {
+            return await _context.Bomons.ToListAsync();
+        }
+        [HttpPost] 
+        public async Task<IActionResult> CreateBomons(Bomon bomoncreate)
+        {
+            _context.Bomons.Add(bomoncreate);
+            await _context.SaveChangesAsync();
+            return StatusCode(201, bomoncreate); 
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateBomons(Bomon bomonupdate,int id)
+        {
+            if(id != bomonupdate.IdBoMon)
+            {
+                return BadRequest(); 
+            }
+            _context.Bomons.Update(bomonupdate);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBomon(int id)
+        {
+            var Bomondel = _context.Khoas.Find(id);
+            _context.Khoas.Remove(Bomondel);
+            await _context.SaveChangesAsync();
+            return StatusCode(200, Bomondel);
+        }
+        [HttpGet("search")]
+        public async Task<IActionResult> SearchBomon([FromQuery] string tenbm)
+        {
+            var kq = await _context.Bomons.Where(bm => bm.TenBoMon.Contains(tenbm)).ToListAsync();
+            return  StatusCode(200,kq); 
+
+        }
+        public int Count { get; set; }
+        public int PageNow { get; set; }
+        public int size { get; set; }
+        public string queryPage { get; set; }
+        [HttpGet("pagination")]
+        public async Task<IEnumerable<Bomon>> pagination( [FromQuery] int? page)
+        {
+            // giống với [fromQuery] là value của 
+            if(page <= 0 || page == null)
+            {
+                page = 1;
+            }
+            size = 1;
+            int pageBymber = (page ?? 1); // toán tử ?? trong c# mô tả nếu khác null thì lấy 
+            // giá trị page . null thì lấy 1 
+            
+            // xem tổng có bao nhiêu trang 
+            PageNow = (int)Math.Ceiling((double)Count / size);
+            var listPages = await  _context.Bomons.Skip(pageBymber * size - size).Take(size).ToListAsync();
+            return listPages; 
+        }
+    }
+}
---
+++ LearnAPI/Controllers/HandleController.cs
@@ -0,0 +1,40 @@
+﻿using LearnAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearnAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HandleController : ControllerBase
+    {
+        private readonly quanlycanbohumgContext _context;
+        public HandleController(quanlycanbohumgContext context)
+        {
+           _context = context; 
+        }
+        [HttpGet("SearchCb/{mcb}")]
+        public async Task<IEnumerable<Canbo>> SearchCanbo(int mcb)
+        {
+            return await _context.Canbos.Where(cb => cb.Idcb == mcb).ToListAsync();
+        }
+        [HttpGet("SearchDV/{mdv}")]
+        public async Task<IEnumerable<Bomon>> SearchDonvi(int mdv)
+        {
+            return await _context.Bomons.Where(dv => dv.IdBoMon == mdv).ToListAsync();
+        }
+        [HttpGet("SearchChucvu")]
+        public async Task<IEnumerable<object>> SearchCanbo([FromQuery] string cvseach)
+        {
+            return await _context.Canbos
+                .Join(_context.Chucvus, cb => cb.Idchucvu, cv => cv.Idchucvucanbo, (cb, cv) => new {  })
+                .Where(x => x.cv.Tenchucvucanbo.Contains(cvseach))
+                .ToListAsync();
+        }
+    }
+}
